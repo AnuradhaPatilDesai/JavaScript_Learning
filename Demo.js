2dc08e3f79b2db384680076e53a9f32159d8c216
AnuradhaPatilDesai <anupatildesai@gmail.com>
2022-10-04 18:36:30 +0530

    First program execution in JS
    First program execution in JS

--- Demo.js
+++ Demo.js
@@ -1,2 +1,7 @@
 console.log("Hello");
 console.log("Patil Anuradha Shivaji")
+console.log("Reading,Writing,")
+console.log("Hardworking,Positive Thinnking,I have good team work spirit")
+console.log("I am not freely communicate to peaple in english language but i am practicing it and I overcome this weakness day by day")
+console.log(30+50)
+console.log(49*479)
