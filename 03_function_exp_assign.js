175a05be91d1c8e6df0c8d39d2811f6fe7c4982b
AnuradhaPatilDesai <anupatildesai@gmail.com>
2022-10-10 14:58:15 +0530

    function expression and string
    function expression and string

---
+++ 03_function_exp_assign.js
@@ -0,0 +1,45 @@
+//area of square
+var square=function(n){
+ console.log(n*n);
+}
+console.log("Square of 5");
+square(5);
+
+console.log("Square of 6");
+square(6);
+console.log("Square of 25");
+square(25);
+console.log("Square of 100");
+square(100);
+console.log("===========================================");
+console.log(typeof square);
+console.log("============================================");
+console.log("calculate area of triangle");
+var areaOfTriangle=function(base,height){
+    console.log( 1/2*(base*height));
+   }
+   console.log("area of triangle");
+   areaOfTriangle(45,34);
+console.log("==============================================");
+console.log("area of reactangle");
+var areaOfrectangle=function(length,width){
+    console.log( length*width);
+   }
+   console.log("area of rectangle");
+   areaOfTriangle(499,917);
+   console.log("===============================================");
+   console.log("Swap values");
+
+  var swap_values= function (arg1, arg2)
+{
+    console.log("==========Before Swap=========");
+    console.log(arg1, arg2);
+    console.log("======After swap=============");
+    var temp=arg1;
+    arg1=arg2;
+    arg2=temp;
+    console.log(arg1, arg2);
+}
+swap_values("Virat", "Anushka");
+swap_values("10000", "2000")
+
---
+++ 04_string_assign.js
@@ -0,0 +1,22 @@
+var given_string="JavaScript the most popular language";
+console.log("JavaScript the most popular language");
+
+var count=given_string.length;
+console.log("Total character available in given string-:",count);
+
+console.log("============================");
+var char=given_string.indexOf('S');
+console.log("index of character S-:",char);
+
+console.log("==============================");
+console.log("index of string loag-:");
+console.log( given_string.indexOf("loag"));
+
+console.log("==================================");
+console.log("last character og string");    
+ console.log(given_string.charAt(given_string.length-1));
+
+ console.log("========================================");
+ console.log("third character og string");    
+ console.log(given_string.charAt(given_string.length-3));
+      
